0d629b3165e6f7c3f5282c20cf9976dd63b673e8
devanshukd04 <91418677+devanshukd04@users.noreply.github.com>
2022-05-20 11:19:56 +0530

    Time: 2 ms (7.65%), Space: 42.6 MB (5.41%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] c=new int[m][n];
+        helper(0,0,c,m,n);
+        return c[0][0];
+    }
+    public List<Integer> helper(int i,int j,int[][] c,int a,int b){
+        List<Integer> a2;
+        List<Integer> a3;
+        if(i==a-1 && j==b-1){
+            c[i][j]=1;
+            a2=new ArrayList<Integer>();
+            a2.add(1);
+            return a2;
+        }
+        if(i>=a || j>=b){
+            a2=new ArrayList<Integer>();
+            a2.add(0);
+            return a2;
+        }
+        if(c[i][j]!=0){
+            a2=new ArrayList<Integer>();
+            a2.add(c[i][j]);
+            return a2;
+        }
+        a2=helper(i+1,j,c,a,b);
+        a3=helper(i,j+1,c,a,b);
+        List<Integer> a4=new ArrayList<Integer>();
+        a4.add(a2.get(0)+a3.get(0));
+        c[i][j]=a2.get(0)+a3.get(0);
+        return a4;
+    }
+}
