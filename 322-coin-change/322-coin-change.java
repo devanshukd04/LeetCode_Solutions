765e286e99a58968e4c6d9832fb8f660fb169c4b
devanshukd04 <91418677+devanshukd04@users.noreply.github.com>
2022-05-21 22:11:10 +0530

    Time: 711 ms (5.00%), Space: 144.5 MB (5.16%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,17 +1,16 @@
 class Solution {
-    int min=-1;
     public int coinChange(int[] coins, int amount) {
     int a;
     Arrays.sort(coins);
-    int[] b=new int[amount+1];
+    HashMap<Integer,Integer> b=new HashMap<Integer,Integer>();
     a=helper(coins,b,amount);
     if(a==-1){
         return -1;
     }
     return a-1;
     }
-    public int helper(int[] coins, int[] b, int amount){
-        int min1=-1;
+    public int helper(int[] coins, HashMap<Integer,Integer> b, int amount){
+        int min=-1;
         int a;
         if(amount==0){
             return 1;
@@ -19,50 +18,29 @@ class Solution {
         if(amount<0){
             return -1;
         }
-        if(b[amount]!=0){
-            if(b[amount]==-1){
+        if(b.containsKey(amount)){
+            if(b.get(amount)==-1){
                 return -1;
             }
-            return b[amount]+1;
+            return b.get(amount)+1;
         }
         for(int i=coins.length-1;i>=0;i--){
             a=helper(coins,b,amount-coins[i]);
             if(a!=-1){
-                if(b[amount]==0 || a<b[amount]){
-                    b[amount]=a;
-                    min1=a;
+                if(!b.containsKey(amount)){
+                    b.put(amount,a);
+                    min=a;
+                }
+                else if(a<b.get(amount)){
+                    b.put(amount,a);
+                    min=a;
                 }
             }
         }
-        if(min1==-1){
-            b[amount]=-1;
+        if(min==-1){
+            b.put(amount,-1);
             return -1;
         }
-        return min1+1;
-        // int a;
-        // int b;
-        // int c;
-        // if(amount==0){
-        //     return count;
-        // }
-        // if(i<0){
-        //     return -1;
-        // }
-        // for(int j=i;j>=0;j--){
-        //     c=amount;
-        //     a=amount/coins[j];
-        //     amount%=coins[j];
-        //     count+=a;
-        //     System.out.println(amount+" "+count+" "+coins[j]);
-        //     b=helper(coins,amount,j-1,count);
-        //     System.out.println(b+" "+amount+" "+count+" "+coins[j]);
-        //     if(b!=-1){
-        //         return b;
-        //     }
-        //     else{
-        //         amount=c;
-        //     }
-        // }
-        // return -1;
+        return min+1;
     }
 }
