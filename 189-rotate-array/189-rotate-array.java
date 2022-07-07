81e9f9c1fdfe6f6ade502ed5fbf46ca64420d334
devanshukd04 <91418677+devanshukd04@users.noreply.github.com>
2022-07-07 13:47:07 +0530

    Time: 1 ms (85.72%), Space: 64.4 MB (53.93%) - LeetHub

--- 189-rotate-array/189-rotate-array.java
+++ 189-rotate-array/189-rotate-array.java
@@ -3,17 +3,18 @@ class Solution {
         int b=nums.length;
         int d,c;
         k%=b;
-        if(k==0){
-            return;
-        }
-        HashMap<Integer,Integer> a=new HashMap<Integer,Integer>();
-        for(int i=0;i<nums.length;i++){
-            a.put(i,nums[i]);
-        }
-        for(int i=0;i<nums.length;i++){
-            d=(i+k)%b;
-            nums[d]=a.get(i);
-            a.remove(i);
+        k=b-k;
+        reverse(nums,0,k-1);
+        reverse(nums,k,b-1);
+        reverse(nums,0,b-1);
+    }
+    public void reverse(int[] nums, int a, int b){
+        while(a<b){
+            int temp=nums[a];
+            nums[a]=nums[b];
+            nums[b]=temp;
+            a++;
+            b--;
         }
     }
 }
