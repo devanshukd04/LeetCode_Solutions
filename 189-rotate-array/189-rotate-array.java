81b19c7e5695701eb3e7ec66a0e2b680a882b81d
devanshukd04 <91418677+devanshukd04@users.noreply.github.com>
2022-07-07 13:42:18 +0530

    Time: 27 ms (5.25%), Space: 71.8 MB (6.11%) - LeetHub

--- 189-rotate-array/189-rotate-array.java
+++ 189-rotate-array/189-rotate-array.java
@@ -1,23 +1,19 @@
 class Solution {
     public void rotate(int[] nums, int k) {
         int b=nums.length;
-        int d;
+        int d,c;
         k%=b;
         if(k==0){
             return;
         }
-        List<Integer> a=new ArrayList<Integer>();
-        int c=b-k;
-        d=c;
-        
-        a.add(nums[c]);
-        c=++c%b;
-        while(c!=d){
-            a.add(nums[c]);
-            c=++c%b;
+        HashMap<Integer,Integer> a=new HashMap<Integer,Integer>();
+        for(int i=0;i<nums.length;i++){
+            a.put(i,nums[i]);
         }
-        for(int i=0;i<a.size();i++){
-            nums[i]=a.get(i);
+        for(int i=0;i<nums.length;i++){
+            d=(i+k)%b;
+            nums[d]=a.get(i);
+            a.remove(i);
         }
     }
 }
