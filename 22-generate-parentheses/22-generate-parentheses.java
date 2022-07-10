6f0f7a21890f7896943de1bd3d6084e907a86718
devanshukd04 <91418677+devanshukd04@users.noreply.github.com>
2022-07-10 18:55:10 +0530

    Time: 16 ms (8.84%), Space: 41.9 MB (99.78%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.java
+++ 22-generate-parentheses/22-generate-parentheses.java
@@ -1,25 +1,31 @@
 class Solution {
     List<String> b=new ArrayList<String>();
     public List<String> generateParenthesis(int n) {
-        genParenthesis(n,0,0,"");
+        StringBuilder a=new StringBuilder();
+        genParenthesis(n,0,0,a);
         return b;
     }
-    public void genParenthesis(int n, int i,int j,String a){
+    public void genParenthesis(int n, int i,int j,StringBuilder a){
         if(i==n && j==n){
-            if(!b.contains(a)){
-                b.add(a);
+            String c=a.toString();
+            if(!b.contains(c)){
+                b.add(c);
             }
             return;
         }
         else if(i==n){
-            genParenthesis(n,i,j+1,a+")");
+            genParenthesis(n,i,j+1,a.append(")"));
+            a.deleteCharAt(a.length()-1);
         }
         else if(i>j){
-            genParenthesis(n,i+1,j,a+"(");
-            genParenthesis(n,i,j+1,a+")");
+            genParenthesis(n,i+1,j,a.append("("));
+            a.deleteCharAt(a.length()-1);
+            genParenthesis(n,i,j+1,a.append(")"));
+            a.deleteCharAt(a.length()-1);
         }
         else if(i==j){
-            genParenthesis(n,i+1,j,a+"(");
+            genParenthesis(n,i+1,j,a.append("("));
+            a.deleteCharAt(a.length()-1);
         }
     }
 }
