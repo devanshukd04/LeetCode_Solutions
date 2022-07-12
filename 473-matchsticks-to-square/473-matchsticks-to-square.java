ef26cc1b6885966294b356a2f5493f8650466cd2
devanshukd04 <91418677+devanshukd04@users.noreply.github.com>
2022-07-12 20:00:23 +0530

    Time: 293 ms (20.34%), Space: 41.9 MB (56.74%) - LeetHub

--- 473-matchsticks-to-square/473-matchsticks-to-square.java
+++ 473-matchsticks-to-square/473-matchsticks-to-square.java
@@ -1,35 +1,49 @@
 class Solution {
     public boolean makesquare(int[] matchsticks) {
-        
-        int total = 0;
-        
-        for (int i : matchsticks) {
-            total += i;
+        int sum=0;
+        int side;
+        Arrays.sort(matchsticks);
+        for(int i=0;i<matchsticks.length/2;i++){
+            int temp=matchsticks[i];
+            sum+=temp;
+            matchsticks[i]=matchsticks[matchsticks.length-1-i];
+            sum+=matchsticks[i];
+            matchsticks[matchsticks.length-1-i]=temp;
         }
-        
-        if (total % 4 != 0) return false; // if we cant make 4 equals sides then theres no way to make a square
-        // sort the array and place the largest sides first. required optimization to not TLE
-        Arrays.sort(matchsticks); 
-        return match(matchsticks, matchsticks.length - 1, 0, 0, 0, 0, total / 4);
+        if(matchsticks.length%2!=0){
+            sum+=matchsticks[matchsticks.length/2];
+        }
+        if(sum%4!=0){
+            return false;
+        }
+        side=sum/4;
+        return validateSquare(0,0,0,0,0,matchsticks,side);
     }
-    
-    public boolean match(int[] matchsticks, int index, int top, int bottom, int left, int right, int target) {
-        
-        if (top == target && bottom == target && left == target && right == target) return true;
-
-        if (top > target || bottom > target || left > target || right > target) return false;
-                        
-        int val = matchsticks[index];
-        
-        boolean t = match(matchsticks, index - 1, top + val, bottom, left, right, target);
-        if (t) return true;
-        boolean b = match(matchsticks, index - 1, top, bottom + val, left, right, target);
-        if (b) return true;
-        boolean l = match(matchsticks, index - 1, top, bottom, left + val, right, target);
-        if (l) return true;
-        boolean r = match(matchsticks, index - 1, top, bottom, left, right + val, target);
-        if (r) return true;
-        
+    public boolean validateSquare(int a,int b,int c,int d,int i, int[] matchsticks,int side){
+        if(a==b && b==c && c==d && d==side){
+            return true;
+        }
+        if(i==matchsticks.length || a>side || b>side || c>side || d>side){
+            return false;
+        }
+        boolean f;
+        int val=matchsticks[i];
+        f=validateSquare(a+val,b,c,d,i+1,matchsticks,side);
+        if(f){
+            return true;
+        }
+        f=validateSquare(a,b+val,c,d,i+1,matchsticks,side);
+        if(f){
+            return true;
+        }
+        f=validateSquare(a,b,c+val,d,i+1,matchsticks,side);
+        if(f){
+            return true;
+        }
+        f=validateSquare(a,b,c,d+val,i+1,matchsticks,side);
+        if(f){
+            return true;
+        }
         return false;
     }
 }
