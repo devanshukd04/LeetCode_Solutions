0572e6619e3b7be717f3654a36dee4a20466c46d
devanshukd04 <91418677+devanshukd04@users.noreply.github.com>
2022-07-15 10:29:41 +0530

    Time: 111 ms (5.14%), Space: 54.2 MB (5.02%) - LeetHub

---
+++ 695-max-area-of-island/695-max-area-of-island.java
@@ -0,0 +1,32 @@
+class Solution {
+    boolean[][] check =new boolean[50][50];
+    int max=0;
+    public int maxAreaOfIsland(int[][] grid) {
+        int ans=0;
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]==1 && check[i][j]==false){
+                    max=0;
+                    adjoiningArea(grid,i,j);
+                    ans=ans>max?ans:max;
+                }
+            }
+        }
+        return ans;
+    }
+    public void adjoiningArea(int[][] grid, int i, int j){
+        if(i>=grid.length || j>=grid[0].length || i<0 || j<0 || check[i][j]==true){
+            return;
+        }
+        else if(grid[i][j]==0){
+            return;
+        }
+        check[i][j]=true;
+        ++max;
+        System.out.println(i+" "+j);
+        adjoiningArea(grid,i+1,j);
+        adjoiningArea(grid,i-1,j);
+        adjoiningArea(grid,i,j+1);
+        adjoiningArea(grid,i,j-1);
+    }
+}
