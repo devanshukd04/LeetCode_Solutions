8c231ce447a9250b52ec16f9130c8a6fc07db18d
devanshukd04 <91418677+devanshukd04@users.noreply.github.com>
2022-07-18 20:25:44 +0530

    Time: 33 ms (53.44%), Space: 92 MB (45.68%) - LeetHub

--- 523-continuous-subarray-sum/523-continuous-subarray-sum.java
+++ 523-continuous-subarray-sum/523-continuous-subarray-sum.java
@@ -3,26 +3,17 @@ class Solution {
         int count=0;
         HashMap<Integer,Integer> map=new HashMap<Integer,Integer>();
         int currsum=0;
-        if(nums.length==1){
-            return false;
-        }
         for(int i=0;i<nums.length;i++){
             currsum+=nums[i];
             int removesum=currsum%k;
             if(map.containsKey(removesum) && map.get(removesum)!=i-1){
                 return true;
             }
-            else if(removesum==0 && nums[1]!=0 && map.containsKey(removesum) && map.get(removesum)==i-1){
+            else if(removesum==0 && i>0){
                 return true;
             }
             if(!map.containsKey(currsum%k)){
                 map.put(currsum%k,i);   
-            }
-            else if(currsum==0 && map.get(0)==1 && nums[0]!=0){
-                map.put(0,i);
-            }
-            if(i==0){
-                map.put(0,1);
             }
         }
         return false;
