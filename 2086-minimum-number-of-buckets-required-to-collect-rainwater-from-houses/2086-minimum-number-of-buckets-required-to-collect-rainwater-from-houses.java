fa0c25b4ca2dd383b2572b458af3fc5a36e0cc2d
devanshukd04 <91418677+devanshukd04@users.noreply.github.com>
2022-07-19 10:13:12 +0530

    Time: 15 ms (43.46%), Space: 47.6 MB (81.31%) - LeetHub

--- 2086-minimum-number-of-buckets-required-to-collect-rainwater-from-houses/2086-minimum-number-of-buckets-required-to-collect-rainwater-from-houses.java
+++ 2086-minimum-number-of-buckets-required-to-collect-rainwater-from-houses/2086-minimum-number-of-buckets-required-to-collect-rainwater-from-houses.java
@@ -1,25 +1,16 @@
 class Solution {
     public int minimumBuckets(String street) {
-        List<Integer> list=new ArrayList<Integer>();
         HashSet<Integer> set=new HashSet<Integer>();
         int count=0;
         if(street.length()==1){
             return street.charAt(0)=='H'?-1:0;
         }
-        int[] check=new int[street.length()];
-        // for(int i=0;i<street.length();i++){
-        //     if(street.charAt(i)=='H'){
-        //         list.add(i);
-        //     }
-        // }
         int i=0;
-        System.out.println(street);
         while(i<street.length()-1){
             if(street.charAt(i)=='H' && street.charAt(i)==street.charAt(i+1)){
                 if(i-1>=0 && street.charAt(i-1)!='H'){
                     if(!set.contains(i-1)){
                         set.add(i-1);
-                        System.out.println(i);
                         ++count;
                     }
                 }
@@ -31,12 +22,10 @@ class Solution {
             else if(street.charAt(i)=='H' && street.charAt(i)!=street.charAt(i+1)){
                 if(!set.contains(i+1)){
                     set.add(i+1);
-                    System.out.println(i);
                     ++count;
                 }
                 i+=2;
             }
-            System.out.println(i+" "+count);
             ++i;
         }
         if(street.charAt(street.length()-1)=='H'){
