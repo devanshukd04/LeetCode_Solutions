8ffdec4414b943efeff8500228f38a9eb0ccd86b
devanshukd04 <91418677+devanshukd04@users.noreply.github.com>
2022-07-19 14:23:10 +0530

    Time: 114 ms (42.58%), Space: 126.4 MB (14.03%) - LeetHub

--- 2333-minimum-sum-of-squared-difference/2333-minimum-sum-of-squared-difference.java
+++ 2333-minimum-sum-of-squared-difference/2333-minimum-sum-of-squared-difference.java
@@ -4,32 +4,30 @@ class Solution {
             return 0;
         }
         long ans=0;
-        int[] arr=new int[(int)1e5+1];
         HashMap<Integer,Integer> map=new HashMap<Integer,Integer>();
         int max=Integer.MIN_VALUE;
         for(int i=0;i<nums1.length;i++){
             int b=Math.abs(nums1[i]-nums2[i]);
-            ++arr[b];
-            ans+=1L*b*b;
             map.put(b,map.getOrDefault(b,0)+1);
+            ans+=1L*b*b;
             max=max<b?b:max;
         }
         int j=max;
         int total=k1+k2;
         while(total>0 && j>0){
-            if(arr[j]>0){
-                long count=arr[j];
+            if(map.getOrDefault(j,0)>0){
+                int count=map.get(j);
                 if(total>=count){
                     ans-=1L*count*((j*j)-((j-1)*(j-1)));
-                    arr[j]=0;
-                    arr[j-1]+=count;
+                    map.remove(j);
+                    map.put(j-1,map.getOrDefault(j-1,0)+count);
                     total-=count;
                 }
                 else{
                     ans-=1L*total*((j*j)-((j-1)*(j-1)));
-                    arr[j-1]+=total;
-                    arr[j]-=total;
-                    return (long)ans;
+                    map.put(j-1,map.getOrDefault(j,0)-total);
+                    map.put(j-1,map.getOrDefault(j-1,0)+total);
+                    return ans;
                 }
             }
             j--;
