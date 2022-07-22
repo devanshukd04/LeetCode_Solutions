3a36147fe5f68b4bc651bd966b179758e4d2d1f0
Devanshu Kishor Dalal <devanshukd27@gmail.com>
2022-07-22 17:28:31 +0530

    Time: 3 ms (55.05%), Space: 44.3 MB (75.80%) - LeetHub

--- 841-keys-and-rooms/841-keys-and-rooms.java
+++ 841-keys-and-rooms/841-keys-and-rooms.java
@@ -1,24 +1,24 @@
 class Solution {
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
         boolean[] arr=new boolean[rooms.size()];
-        visitRooms(arr,rooms,0);
+        Stack<Integer> stack=new Stack<Integer>();
+        stack.push(0);
+        arr[0]=true;
+        while(!stack.isEmpty()){
+            int a=stack.pop();
+            List<Integer> list=rooms.get(a);
+            for(int num:list){
+                if(!arr[num]){
+                    arr[num]=true;
+                    stack.push(num);
+                }
+            }
+        }
         for(boolean a:arr){
             if(!a){
                 return false;
             }
         }
         return true;
-    }
-    public void visitRooms(boolean[] arr,List<List<Integer>> rooms,int i){
-        List<Integer> list=rooms.get(i);
-        arr[i]=true;
-        int j=0;
-        while(j<list.size()){
-            if(!arr[list.get(j)]){
-                visitRooms(arr,rooms,list.get(j));
-            }
-            ++j;
-        }
-        return;
     }
 }
