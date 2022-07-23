f752e687d9401cfe002bc0be6b575e7a3e208322
Devanshu Kishor Dalal <devanshukd27@gmail.com>
2022-07-23 19:12:54 +0530

    Time: 24 ms (99.76%), Space: 113.6 MB (81.82%) - LeetHub

---
+++ 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<Integer> countSmaller(int[] nums) {
+        int min=Integer.MAX_VALUE;
+        int max=Integer.MIN_VALUE;
+        for(int num:nums){
+            min=Math.min(min,num);
+            max=Math.max(max,num);
+        }
+        min--;
+        int[] count=new int[max-min+1];
+        Integer[] result=new Integer[nums.length];
+        for(int i=nums.length-1;i>=0;i--){
+            int k=nums[i]-min-1;
+            int c=0;
+            do{
+                c+=count[k];
+                k-=(-k&k);
+            }while(k>0);
+            result[i]=c;
+            
+            k=nums[i]-min;
+            while(k<count.length){
+                count[k]++;
+                k+=(-k&k);
+            }
+        }
+        return Arrays.asList(result);
+    }
+}
