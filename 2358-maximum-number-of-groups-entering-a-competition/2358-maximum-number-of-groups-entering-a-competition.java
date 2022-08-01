e08e0dd41dcaf212bc9608b603dc484c6cf688a6
Devanshu Kishor Dalal <devanshukd27@gmail.com>
2022-08-01 19:38:13 +0530

    Time: 38 ms (10.00%), Space: 70.6 MB (10.00%) - LeetHub

--- 2358-maximum-number-of-groups-entering-a-competition/2358-maximum-number-of-groups-entering-a-competition.java
+++ 2358-maximum-number-of-groups-entering-a-competition/2358-maximum-number-of-groups-entering-a-competition.java
@@ -1,21 +1,17 @@
 class Solution {
     public int maximumGroups(int[] grades) {
         Arrays.sort(grades);
-        int count=0;
         int group_size=1;
-        int sum=0;
         int i=0;
         while(i<grades.length){
             if(i+group_size<=grades.length){
                 i+=group_size;
-                sum+=group_size;
                 group_size++;
-                count++;   
             }
             else{
                 break;
             }
         }
-        return count;
+        return --group_size;
     }
 }
