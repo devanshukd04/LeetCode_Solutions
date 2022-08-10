8ff0baafe67ea1877d9c897151cb19b383b450c8
Devanshu Kishor Dalal <devanshukd27@gmail.com>
2022-08-10 19:05:30 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (19.20%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -1,8 +1,14 @@
 class Solution {
     public int rob(int[] nums) {
         int[] dp=new int[nums.length];
-        Arrays.fill(dp,-1);
-        return rob(nums,dp,nums.length-1);
+        dp[0]=nums[0];
+        int neg=0;
+        for(int i=1;i<nums.length;i++){
+            int house1=nums[i]+((i-2)<0?0:dp[i-2]);
+            int house2=dp[i-1];
+            dp[i]=Math.max(house1,house2);
+        }
+        return dp[nums.length-1];
     }
     public int rob(int[] nums,int[] dp,int i){
         if(i==0){
