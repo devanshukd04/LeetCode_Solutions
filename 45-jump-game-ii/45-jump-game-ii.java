d48d4f10c7a72ff59b3b4d43c64508465626972c
Devanshu Kishor Dalal <devanshukd27@gmail.com>
2022-08-14 10:43:02 +0530

    Time: 64 ms (34.04%), Space: 50 MB (21.34%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.java
+++ 45-jump-game-ii/45-jump-game-ii.java
@@ -2,18 +2,18 @@ class Solution {
     public int jump(int[] nums) {
         int[] dp=new int[nums.length];
         Arrays.fill(dp,(int)1e4+1);
-        return helper(nums,0,dp);
-    }
-    public int helper(int[] nums,int i,int[] dp){
-        if(i>=nums.length-1){
-            return 0;
+        dp[0]=0;
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<=i+nums[i];j++){
+                if(j<nums.length){
+                    dp[j]=Math.min(dp[j],dp[i]+1);
+                }
+                else{
+                    dp[nums.length-1]=Math.min(dp[nums.length-1],dp[i]+1);
+                }
+            }
         }
-        if(dp[i]!=(int)1e4+1){
-            return dp[i];
-        }
-        for(int j=i+1;j<=i+nums[i];j++){
-            dp[i]=Math.min(dp[i],1+helper(nums,j,dp));
-        }
-        return dp[i];
+        return dp[nums.length-1];
+        
     }
 }
