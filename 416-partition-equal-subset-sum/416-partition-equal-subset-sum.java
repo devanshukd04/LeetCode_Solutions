a1b6e5296db8e24d48a386e9f4ffebae15495ee9
Devanshu Kishor Dalal <devanshukd27@gmail.com>
2022-08-17 14:34:35 +0530

    Time: 70 ms (52.11%), Space: 59 MB (36.38%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
@@ -4,23 +4,26 @@ class Solution {
         for(int num:nums){
             sum+=num;
         }
-        int[][] dp=new int[nums.length][sum];
+        int[][] dp=new int[nums.length][sum/2+1];
         for(int i=0;i<nums.length;i++){
             Arrays.fill(dp[i],-1);
         }
-        return partition(0,nums,0,0,dp);
+        return partition(0,nums,0,0,dp,sum/2);
     }
-    public boolean partition(int index,int[] nums,int sum1,int sum2,int[][] dp){
+    public boolean partition(int index,int[] nums,int sum1,int sum2,int[][] dp,int sum){
         if(index==nums.length){
             if(sum1==sum2){
                 return true;
             }
             return false;
         }
+        if(sum1>sum || sum2>sum){
+            return false;
+        }
         if(dp[index][sum1]!=-1){
             return dp[index][sum1]==1?true:false;
         }
-        boolean a=partition(index+1,nums,sum1+nums[index],sum2,dp) || partition(index+1,nums,sum1,sum2+nums[index],dp);
+        boolean a=partition(index+1,nums,sum1+nums[index],sum2,dp,sum) || partition(index+1,nums,sum1,sum2+nums[index],dp,sum);
         if(a){
             dp[index][sum1]=1;
         }
