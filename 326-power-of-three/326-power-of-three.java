0ac19caeef902ded5de8790c9f6a732160b0202e
Devanshu Kishor Dalal <devanshukd27@gmail.com>
2022-08-24 09:31:09 +0530

    Time: 22 ms (62.48%), Space: 47.4 MB (38.69%) - LeetHub

--- 326-power-of-three/326-power-of-three.java
+++ 326-power-of-three/326-power-of-three.java
@@ -1,5 +1,15 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
-        return (Math.log10(n)/Math.log10(3))%1==0;
+        if(n<=0){
+            return false;
+        }
+        long temp=1;
+        while(temp<n){
+            temp+=temp<<1;
+        }
+        if(temp==n){
+            return true;
+        }
+        return false;
     }
 }
