ef493c7f0318829c832fc9fa58c4ce445d524127
Devanshu Kishor Dalal <devanshukd27@gmail.com>
2022-08-24 15:54:40 +0530

    Time: 0 ms (100.00%), Space: 39.5 MB (97.17%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.java
+++ 213-house-robber-ii/213-house-robber-ii.java
@@ -6,23 +6,23 @@ class Solution {
         if(nums.length==2){
             return Math.max(nums[0],nums[1]);
         }
-        int[] dp=new int[nums.length+1];
-        dp[0]=nums[0];
-        dp[1]=Math.max(nums[0],nums[1]);
-        for(int i=2;i<nums.length-1;i++){
-            dp[i]=Math.max(nums[i]+dp[i-2],dp[i-1]);
+        int prev=nums[0];
+        int prev2=0;
+        for(int i=1;i<nums.length-1;i++){
+            int current=Math.max(nums[i]+prev2,prev);
+            prev2=prev;
+            prev=current;
         }
-        dp[nums.length-1]=Math.max(dp[nums.length-2],dp[nums.length-3]);
-        int ans1=dp[nums.length-1];
+        int ans1=Math.max(prev,prev2);
         
-        Arrays.fill(dp,0);
-        dp[1]=nums[1];
-        dp[2]=Math.max(nums[1],nums[2]);
-        for(int i=3;i<nums.length;i++){
-            dp[i]=Math.max(nums[i]+dp[i-2],dp[i-1]);
+        prev=nums[1];
+        prev2=0;
+        for(int i=2;i<nums.length;i++){
+            int current=Math.max(nums[i]+prev2,prev);
+            prev2=prev;
+            prev=current;
         }
-        dp[nums.length]=Math.max(dp[nums.length-1],dp[nums.length-2]);
-        int ans2=dp[nums.length];
+        int ans2=Math.max(prev,prev2);
         
         return Math.max(ans1,ans2);
     }
