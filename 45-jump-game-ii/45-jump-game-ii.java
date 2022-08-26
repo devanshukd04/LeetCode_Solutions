2a51b4066f81496165e38f50466c823909a9ac6c
Devanshu Kishor Dalal <devanshukd27@gmail.com>
2022-08-26 14:55:47 +0530

    Time: 165 ms (16.37%), Space: 43.2 MB (86.47%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.java
+++ 45-jump-game-ii/45-jump-game-ii.java
@@ -1,19 +1,21 @@
 class Solution {
     public int jump(int[] nums) {
-        int[] dp=new int[nums.length];
-        Arrays.fill(dp,(int)1e4+1);
-        dp[0]=0;
-        for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<=i+nums[i];j++){
-                if(j<nums.length){
-                    dp[j]=Math.min(dp[j],dp[i]+1);
-                }
-                else{
-                    dp[nums.length-1]=Math.min(dp[nums.length-1],dp[i]+1);
-                }
-            }
+        int[] dp=new int[nums.length+1];
+        Arrays.fill(dp,-1);
+        return jumpGame(nums,0,dp);
+    }
+    public int jumpGame(int[] nums, int i,int[] dp){
+        int ans=Integer.MAX_VALUE;
+        if(i>=nums.length-1){
+            dp[nums.length-1]=1;
+            return 0;
         }
-        return dp[nums.length-1];
-        
+        if(dp[i]!=-1){
+            return dp[i];
+        }
+        for(int j=i+1;j<=i+nums[i];j++){
+            ans=Math.min(ans,jumpGame(nums,j,dp)+1);
+        }
+        return dp[i]=ans==Integer.MAX_VALUE?Integer.MAX_VALUE-100:ans;
     }
 }
