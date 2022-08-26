13d2c58018da5b1cd185efa98ba2451bf28435cb
Devanshu Kishor Dalal <devanshukd27@gmail.com>
2022-08-26 15:27:30 +0530

    Time: 345 ms (38.53%), Space: 0B (100.00%) - LeetHub

---
+++ 176-second-highest-salary/176-second-highest-salary.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select IFNULL((select salary from Employee where salary<(select max(salary) from Employee) order by salary desc limit 1),null) as SecondHighestSalary;
