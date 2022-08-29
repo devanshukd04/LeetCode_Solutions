7f317f53753ab7f2dec65cde5a7b405b17e0099a
Devanshu Kishor Dalal <devanshukd27@gmail.com>
2022-08-29 18:46:53 +0530

    Time: 2551 ms (11.94%), Space: 0B (100.00%) - LeetHub

--- 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -1,3 +1,2 @@
 # Write your MySQL query statement below
-select customer_id , count(distinct visit_id) as count_no_trans
-from visits where visit_id not in (select distinct visit_id from transactions) group by customer_id;
+select customer_id, count(distinct visit_id) as count_no_trans from visits where visit_id not in (select distinct visit_id from transactions) group by customer_id;
