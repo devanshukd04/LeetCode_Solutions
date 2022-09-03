bc0d62f39d8dedfdd73b10d8fb6614db91c3a5d6
Devanshu Kishor Dalal <devanshukd27@gmail.com>
2022-09-03 11:24:27 +0530

    Time: 3 ms (80.94%), Space: 43.2 MB (36.25%) - LeetHub

--- 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.java
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.java
@@ -1,25 +1,35 @@
 class Solution {
     public int[] numsSameConsecDiff(int n, int k) {
+        //Creating a list to store ans
         List<Integer> ans=new ArrayList<Integer>();
+        //Using for loop as first digit cannot be 0
         for(int i=1;i<10;i++){
             digitDiff(n-1,k,ans,i);
         }
+        //As return type is integer array creating a array
         int[] res=new int[ans.size()];
         int j=0;
+        //Storing the elements of list into an integer array
         for(int num:ans){
             res[j++]=num;
         }
+        //Returning the array
         return res;
     }
     public void digitDiff(int n,int k,List<Integer> ans,int num){
+        //If num equals 0, implies we have foyund one number
         if(n==0){
             ans.add(num);
             return;
         }
+        //Accessing the last digit by num%10
         int digit=num%10;
+        //If digit-k>=0 means we can take that digit
         if(digit-k>=0){
             digitDiff(n-1,k,ans,num*10+(digit-k));
         }
+        //If digit+k<=9 means we can take that digit
+        //There's a check that digit-k!=digit+k this case occurs to avoid repeated function calls when k is 0
         if(digit+k<=9 && digit-k!=digit+k){
             digitDiff(n-1,k,ans,num*10+(digit+k));
         }
