da522dffcb2901ed5a09ea5c88be869eb9fafeba
Devanshu Kishor Dalal <devanshukd27@gmail.com>
2022-09-03 18:40:15 +0530

    Update 42-trapping-rain-water.java

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -75,3 +75,24 @@ class Solution {
         return sum;
     }
 }
+
+
+
+//More Optimized Solution
+
+// int[] left=new int[height.length];
+// int[] right=new int[height.length];
+// int ans=0;
+// left[0]=Integer.MIN_VALUE;
+// for(int i=1;i<height.length;i++){
+//     left[i]=Math.max(left[i-1],height[i-1]);
+// }
+// right[height.length-1]=Integer.MIN_VALUE;
+// for(int i=height.length-2;i>=0;i--){
+//     right[i]=Math.max(right[i+1],height[i+1]);
+// }
+// for(int i=1;i<height.length-1;i++){
+//     int curr=Math.min(left[i],right[i])-height[i];
+//     ans+=curr>0?curr:0;
+// }
+// return ans;
