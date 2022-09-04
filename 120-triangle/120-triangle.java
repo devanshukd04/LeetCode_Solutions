4af173e544e74fe577ab7e74f724b825fbd821bf
Devanshu Kishor Dalal <devanshukd27@gmail.com>
2022-09-04 17:03:23 +0530

    Update 120-triangle.java

--- 120-triangle/120-triangle.java
+++ 120-triangle/120-triangle.java
@@ -14,3 +14,30 @@ class Solution {
         return b[0];
     }
 }
+
+//Dynamic Programming
+
+// class Solution {
+//     public int minimumTotal(List<List<Integer>> triangle) {
+//         int[][] dp=new int[triangle.size()][triangle.get(triangle.size()-1).size()];
+//         int[][] visited=new int[triangle.size()][triangle.get(triangle.size()-1).size()];
+//         for(int i=0;i<dp.length;i++){
+//             Arrays.fill(dp[i],-1);
+//         }
+//         return helper(triangle,0,0,dp,visited);
+//     }
+//     public int helper(List<List<Integer>> triangle,int i,int j,int[][] dp,int[][] visited){
+//         if(visited[i][j]!=0){
+//             return dp[i][j];
+//         }
+//         visited[i][j]=1;
+//         if(i<triangle.size()-1){
+//             int a=helper(triangle,i+1,j,dp,visited);
+//             int b=helper(triangle,i+1,j+1,dp,visited);
+//             return dp[i][j]=triangle.get(i).get(j)+Math.min(a,b);
+//         }
+//         else{
+//             return dp[i][j]=triangle.get(i).get(j);
+//         }
+//     }
+// }
