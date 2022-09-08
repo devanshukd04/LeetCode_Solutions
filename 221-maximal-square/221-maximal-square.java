ddc52da0b36884263768324dba526eb9c69208f1
Devanshu Kishor Dalal <devanshukd27@gmail.com>
2022-09-08 15:04:49 +0530

    Time: 16 ms (17.87%), Space: 66.8 MB (16.97%) - LeetHub

--- 221-maximal-square/221-maximal-square.java
+++ 221-maximal-square/221-maximal-square.java
@@ -3,11 +3,26 @@ class Solution {
         int ans=0;
         int[][] dp=new int[matrix.length][matrix[0].length];
         for(int[] arr:dp){
-            Arrays.fill(arr,-1);
+            Arrays.fill(arr,0);
+        }
+        for(int i=0;i<matrix[0].length;i++){
+            if(matrix[0][i]=='1'){
+                dp[0][i]=1;
+                ans=dp[0][i];
+            }
         }
         for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[0].length;j++){
-                ans=Math.max(ans,maximalSquareAns(matrix,dp,i,j));
+            if(matrix[i][0]=='1'){
+                dp[i][0]=1;
+                ans=dp[i][0];
+            }
+        }
+        for(int i=1;i<matrix.length;i++){
+            for(int j=1;j<matrix[0].length;j++){
+                if(matrix[i][j]=='1'){
+                    dp[i][j]=Math.min(dp[i-1][j-1],Math.min(dp[i-1][j],dp[i][j-1]))+1;
+                    ans=Math.max(ans,dp[i][j]);   
+                }
             }
         }
         return ans*ans;
