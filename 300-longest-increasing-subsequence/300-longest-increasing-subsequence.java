cc873b6e6f4b3aba9584b64c226b48da9c406b21
Devanshu Kishor Dalal <devanshukd27@gmail.com>
2022-09-10 17:48:31 +0530

    Update 300-longest-increasing-subsequence.java

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -21,3 +21,25 @@ class Solution {
         return dp[i][prev+1]=Math.max(take,notTake);
     }
 }
+
+
+// class Solution {
+//     public int lengthOfLIS(int[] nums) {
+//         int[] dp=new int[nums.length];
+//         dp[0]=1;
+//         for(int i=1;i<nums.length;i++){
+//             int max=1;
+//             for(int j=0;j<i;j++){
+//                 if(nums[i]>nums[j]){
+//                     max=Math.max(max,dp[j]+1);
+//                 }
+//             }
+//             dp[i]=max;
+//         }
+//         int max=1;
+//         for(int x:dp){
+//             max=Math.max(max,x);
+//         }
+//         return max;
+//     }
+// }
