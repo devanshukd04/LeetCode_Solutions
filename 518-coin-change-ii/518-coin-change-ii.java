132678d998884004b49d71995976ccf200a03281
Devanshu Kishor Dalal <devanshukd27@gmail.com>
2022-09-11 12:47:01 +0530

    Time: 35 ms (13.46%), Space: 51.5 MB (18.55%) - LeetHub

--- 518-coin-change-ii/518-coin-change-ii.java
+++ 518-coin-change-ii/518-coin-change-ii.java
@@ -1,11 +1,19 @@
 class Solution {
     public int change(int amount, int[] coins) {
         Arrays.sort(coins);
-        int[][] dp=new int[amount+1][coins.length];
-        for(int[] arr:dp){
-            Arrays.fill(arr,-1);
+        int[][] dp=new int[amount+1][coins.length+1];
+        for(int i=0;i<coins.length+1;i++){
+            dp[0][i]=1;
         }
-        return helper(amount,coins,dp,coins.length-1);
+        for(int i=1;i<=amount;i++){
+            for(int j=1;j<=coins.length;j++){
+                if(i-coins[j-1]>=0){
+                    dp[i][j]+=dp[i-coins[j-1]][j];    
+                }
+                dp[i][j]+=dp[i][j-1];
+            }
+        }
+        return dp[amount][coins.length];
     }
     public int helper(int amount,int[] coins,int[][] dp,int i){
         if(amount==0){
