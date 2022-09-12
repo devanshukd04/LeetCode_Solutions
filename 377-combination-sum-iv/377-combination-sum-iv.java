f8b8e83b657bd3e21ad10722562b18d47e5330de
Devanshu Kishor Dalal <devanshukd27@gmail.com>
2022-09-12 16:56:43 +0530

    Time: 1 ms (89.87%), Space: 41.4 MB (59.54%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.java
+++ 377-combination-sum-iv/377-combination-sum-iv.java
@@ -1,25 +1,29 @@
 class Solution {
     public int combinationSum4(int[] nums, int target) {
-        Integer[] memo=new Integer[target+1];
-        return recurse(nums,target,memo);
+        Arrays.sort(nums);
+        int[] dp=new int[target+1];
+        Arrays.fill(dp,-1);
+        return helper(nums,dp,target);
     }
-    public int recurse(int[] nums,int remain,Integer[] memo){
-        if(remain<0){
+    public int helper(int[] nums,int[] dp,int target){
+        if(target==0){
+            return 1;
+        }
+        if(target<0){
             return 0;
         }
-        if(memo[remain]!=null){
-            return memo[remain];
-        }
-        if(remain==0){
-            return 1;
+        if(dp[target]!=-1){
+            return dp[target];
         }
         int ans=0;
         for(int i=0;i<nums.length;i++){
-            if(remain-nums[i]>=0){
-                ans+=recurse(nums,remain-nums[i],memo);   
+            if(nums[i]<=target){
+                ans+=helper(nums,dp,target-nums[i]);
+            }
+            else{
+                return dp[target]=ans;
             }
         }
-        memo[remain]=ans;
-        return memo[remain];
+        return dp[target]=ans;
     }
 }
