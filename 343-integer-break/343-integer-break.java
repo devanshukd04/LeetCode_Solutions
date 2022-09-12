f9b7af15352f5ef92a00729fedb9e1e66d60f3e8
Devanshu Kishor Dalal <devanshukd27@gmail.com>
2022-09-12 16:35:05 +0530

    Time: 1 ms (67.93%), Space: 41.3 MB (19.64%) - LeetHub

--- 343-integer-break/343-integer-break.java
+++ 343-integer-break/343-integer-break.java
@@ -1,22 +1,26 @@
 class Solution {
     public int integerBreak(int n) {
+        int[][] dp=new int[n+1][n+1];
         if(n==2 || n==3){
             return n==2?1:2;
         }
-        return helper(n,1);
-    }
-    public int helper(int n,int i){
-        if(n==0){
-            return 1;
+        for(int[] arr:dp){
+            Arrays.fill(arr,-1);
         }
-        if(n==1){
+        return helper(n,dp,1);
+    }
+    public int helper(int n,int[][] dp,int i){
+        if(n==0){
             return 1;
         }
         if(i>n || n<0){
             return 0;
         }
-        int notTake=helper(n,i+1);
-        int take=i*helper(n-i,i);
-        return Math.max(take,notTake);
+        if(dp[n][i]!=-1){
+            return dp[n][i];
+        }
+        int notTake=helper(n,dp,i+1);
+        int take=i*helper(n-i,dp,i);
+        return dp[n][i]=Math.max(take,notTake);
     }
 }
