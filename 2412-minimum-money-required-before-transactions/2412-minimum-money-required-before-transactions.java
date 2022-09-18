378e60249f5bb0eabf30c671de3a208c08dfff05
Devanshu Kishor Dalal <devanshukd27@gmail.com>
2022-09-18 19:13:15 +0530

    Time: 38 ms (16.67%), Space: 129.2 MB (50.00%) - LeetHub

--- 2412-minimum-money-required-before-transactions/2412-minimum-money-required-before-transactions.java
+++ 2412-minimum-money-required-before-transactions/2412-minimum-money-required-before-transactions.java
@@ -1,11 +1,20 @@
 class Solution {
     public long minimumMoney(int[][] transactions) {
         long max=0;
-        long res=0;
+        long ans=0;
+        long buffer=0;
+        Arrays.sort(transactions,(int[] a,int[] b)->(a[1]-b[1]));
         for(int[] arr:transactions){
-            max=Math.max(max,Math.min(arr[0],arr[1]));
-            res+=Math.max(arr[0]-arr[1],0);
+            if(arr[0]>arr[1]){
+                max+=arr[0];
+                ans=Math.max(ans,max);
+                max-=arr[1];
+            }
+            else{
+                buffer=Math.max(buffer,arr[0]);
+            }
         }
-        return res+max;
+        ans=Math.max(ans,max+buffer);
+        return ans; 
     }
 }
