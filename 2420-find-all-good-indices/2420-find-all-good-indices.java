28d1aa718e8d1187ed816bfd0f0caaa5c76c0ef0
Devanshu Kishor Dalal <devanshukd27@gmail.com>
2022-09-25 09:40:55 +0530

    Time: 2169 ms (16.67%), Space: 112.6 MB (16.67%) - LeetHub

--- 2420-find-all-good-indices/2420-find-all-good-indices.java
+++ 2420-find-all-good-indices/2420-find-all-good-indices.java
@@ -3,6 +3,7 @@ class Solution {
         List<Integer> ans=new ArrayList<Integer>();
         boolean flag1=true;
         boolean flag2=true;
+        //If all elements are equal the return nums.length-2*k 
         HashSet<Integer> set=new HashSet<Integer>();
         List<Integer> res=new ArrayList<Integer>();
         for(int i=0;i<nums.length;i++){
@@ -17,12 +18,14 @@ class Solution {
         for(int i=k;i<nums.length-k;i++){
             flag1=true;
             flag2=true;
+            //Check if index-k upto index is non-increasing
             for(int j=i-k;j<i-1;j++){
                 if(nums[j]-nums[j+1]<0){
                     flag1=false;
                     break;
                 }
             }
+            //Check if index upto index+k is non-decreasing
             if(flag1){
                 for(int j=i+1;j<i+k;j++){
                     if(nums[j]-nums[j+1]>0){
@@ -31,6 +34,7 @@ class Solution {
                     }
                 }   
             }
+            //If condition satisfis then add the index in list
             if(flag1 && flag2){
                 ans.add(i);
             }
