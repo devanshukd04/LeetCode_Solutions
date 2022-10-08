2b4ab2d00748b7258c56a4692416f35c56767b1f
Devanshu Kishor Dalal <devanshukd27@gmail.com>
2022-10-08 10:57:22 +0530

    Time: 331 ms (5.05%), Space: 173.1 MB (13.46%) - LeetHub

---
+++ 2424-longest-uploaded-prefix/2424-longest-uploaded-prefix.java
@@ -0,0 +1,27 @@
+class LUPrefix {
+    TreeSet<Integer> set;
+    int n;
+    public LUPrefix(int n) {
+        set=new TreeSet<Integer>();
+        for(int i=1;i<=n;i++){
+            this.set.add(i);
+        }
+        System.out.println(this.set);
+        this.n=n;
+    }
+    
+    public void upload(int video) {
+        this.set.remove(video);
+    }
+    
+    public int longest() {
+        return this.set.isEmpty()?this.n:this.set.first()-1;
+    }
+}
+
+/**
+ * Your LUPrefix object will be instantiated and called as such:
+ * LUPrefix obj = new LUPrefix(n);
+ * obj.upload(video);
+ * int param_2 = obj.longest();
+ */
