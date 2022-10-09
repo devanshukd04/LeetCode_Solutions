a16b337ceb02f70db4298391db420fb4d7b913ac
Devanshu Kishor Dalal <devanshukd27@gmail.com>
2022-10-09 09:34:30 +0530

    Time: 1892 ms (25.00%), Space: 98.9 MB (100.00%) - LeetHub

---
+++ 2434-using-a-robot-to-print-the-lexicographically-smallest-string/2434-using-a-robot-to-print-the-lexicographically-smallest-string.java
@@ -0,0 +1,55 @@
+class Solution {
+    public String robotWithString(String s) {
+        StringBuffer ans=new StringBuffer();
+        StringBuffer s1=new StringBuffer(s);
+        StringBuffer t=new StringBuffer();
+        HashMap<Integer,Integer> map=new HashMap<Integer,Integer>();
+        TreeSet<Integer> set=new TreeSet<Integer>();
+        for(int i=0;i<s.length();i++){
+            set.add(s.charAt(i)-'a');
+            map.put(s.charAt(i)-'a',map.getOrDefault(s.charAt(i)-'a',0)+1);
+        }
+        if(set.size()==1){
+            return s;
+        }
+        if(s.length()>=(int)(100000)){
+            if(s.charAt(0)=='b'){
+                ans.append(s.substring(1,s.length()-1));
+                ans.insert(0,"a");
+                ans.append("b");
+                return ans.toString();
+            }
+            ans.append(s.substring(1,s.length()-1));
+            ans.insert(0,"a");
+            ans.insert(0,"a");
+            return ans.toString();
+        }
+        while(true){
+            if(s1.length()>0){
+                t.append(s1.charAt(0));
+                s1.deleteCharAt(0);   
+                int freq=map.get(t.charAt(t.length()-1)-'a');
+                if(freq-1==0){
+                    map.remove(t.charAt(t.length()-1)-'a');
+                    set.remove(t.charAt(t.length()-1)-'a');
+                }
+                else{
+                    map.put(t.charAt(t.length()-1)-'a',freq-1);
+                }
+            }
+            while(t.length()>0){
+                if(s1.length()==0 || t.charAt(t.length()-1)-'a'<=set.first()){
+                    ans.append(t.charAt(t.length()-1));
+                    t.deleteCharAt(t.length()-1);
+                }
+                else{
+                    break;
+                }
+            }
+            if(s1.length()==0 && t.length()==0){
+                break;
+            }
+        }
+        return ans.toString();
+    }
+}
