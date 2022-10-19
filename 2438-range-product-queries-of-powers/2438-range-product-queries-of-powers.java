01eeaac14cbbc2cf54a1c74899f7fc3731d78e63
Devanshu Kishor Dalal <devanshukd27@gmail.com>
2022-10-19 10:49:40 +0530

    Time: 75 ms (59.99%), Space: 135.1 MB (22.33%) - LeetHub

--- 2438-range-product-queries-of-powers/2438-range-product-queries-of-powers.java
+++ 2438-range-product-queries-of-powers/2438-range-product-queries-of-powers.java
@@ -10,11 +10,6 @@ class Solution {
             bit*=2;
             n>>=1;
         }
-        int[] degree=new int[power.size()];
-        degree[0]=power.get(0);
-        for(int i=1;i<degree.length;i++){
-            degree[i]=(degree[i-1]*power.get(i))%((int)1e9+7);
-        }
         for(int i=0;i<queries.length;i++){
             long res=1;
             for(int j=queries[i][0];j<=queries[i][1];j++){
