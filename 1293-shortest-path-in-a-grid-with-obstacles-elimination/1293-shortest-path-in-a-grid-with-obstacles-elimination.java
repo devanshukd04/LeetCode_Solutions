85ed73ffbfa5dcbf1274699b94b25887443d3f17
Devanshu Kishor Dalal <devanshukd27@gmail.com>
2022-10-30 19:45:48 +0530

    Time: 30 ms (72.05%), Space: 54.7 MB (36.25%) - LeetHub

---
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int shortestPath(int[][] grid, int k) {
+        int[][][] dp=new int[grid.length][grid[0].length][k+1];
+        for(int[][] matrix:dp){
+            for(int[] row:matrix){
+                Arrays.fill(row,Integer.MAX_VALUE);
+            }
+        }
+        int ans=helper(grid,grid.length-1,grid[0].length-1,k,dp);
+        return ans==Integer.MAX_VALUE?-1:ans;
+    }
+    public int helper(int[][] grid,int i,int j,int k,int[][][] dp){
+        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || grid[i][j]>1){
+            return Integer.MAX_VALUE;
+        }
+        if(i==0 && j==0){
+            return 0;
+        }
+        if(dp[i][j][k]!=Integer.MAX_VALUE){
+            return dp[i][j][k];
+        }
+        if(k>=i+j){
+            return dp[i][j][k]=i+j;
+        }
+        if(grid[i][j]==1 && --k<0){
+            return Integer.MAX_VALUE;
+        }
+        int oldValue=grid[i][j];
+        grid[i][j]=2;
+        int minWays=Integer.MAX_VALUE;
+        minWays=Math.min(minWays,helper(grid,i+1,j,k,dp));
+        minWays=Math.min(minWays,helper(grid,i,j+1,k,dp));
+        minWays=Math.min(minWays,helper(grid,i-1,j,k,dp));
+        minWays=Math.min(minWays,helper(grid,i,j-1,k,dp));
+        grid[i][j]=oldValue;
+        minWays=minWays==Integer.MAX_VALUE?minWays:minWays+1;
+        return dp[i][j][k]=minWays;
+    }
+}
