7029a343bde032159a68dde2310315190b727c0d
Devanshu Kishor Dalal <devanshukd27@gmail.com>
2023-01-21 23:36:21 +0530

    Time: 11 ms (33.33%), Space: 91.8 MB (11.11%) - LeetHub

---
+++ 2541-minimum-operations-to-make-array-equal-ii/2541-minimum-operations-to-make-array-equal-ii.java
@@ -0,0 +1,32 @@
+class Solution {
+    public long minOperations(int[] nums1, int[] nums2, int k) {
+        long ans=0;
+        
+        long sum=0;
+        if(k==0){
+            for(int i=0;i<nums1.length;i++){
+                if(nums1[i]-nums2[i]!=0){
+                    return -1;
+                }
+            }
+            return 0;
+        }
+        for(int i=0;i<nums1.length;i++){
+            int diff=nums1[i]-nums2[i];
+            if(diff==0){
+                continue;
+            }
+            if(diff%k!=0){
+                return -1;
+            }
+            ans+=Math.abs(diff)/k;
+            sum+=diff;
+        }
+        if(sum!=0){
+            return -1;
+        }
+        
+        
+        return ans/2;
+    }
+}
