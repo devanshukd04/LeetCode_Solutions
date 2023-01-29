24f729ddbbf07f7cd0939b0bbd6a5c9a2dfb3da9
Devanshu Kishor Dalal <devanshukd27@gmail.com>
2023-01-29 10:33:19 +0530

    Added solution - LeetHub

---
+++ Min operations - GFG/min-operations.java
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int a;
+            a = Integer.parseInt(br.readLine());
+            
+            
+            int b;
+            b = Integer.parseInt(br.readLine());
+            
+            Solution obj = new Solution();
+            int res = obj.solve(a, b);
+            
+            out.println(res);
+            
+        }
+        out.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public static int solve(int a, int b) {
+        // code here
+        if(a==b){
+            return 0;
+        }
+        int temp=a&b;
+        if(temp==a || temp==b){
+            return 1;
+        }
+        return 2;
+    }
+}
+        
