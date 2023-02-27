e2c1150e8a6ecbd664b9a932fbae0213de34fdbb
Devanshu Kishor Dalal <devanshukd27@gmail.com>
2023-02-27 22:07:58 +0530

    Added solution - LeetHub

---
+++ Reverse both parts - GFG/reverse-both-parts.java
@@ -0,0 +1,112 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class Node
+{
+    int data;
+    Node next;
+
+    Node(int x)
+    {
+        data = x;
+        next = null;
+    }
+
+    public static Node inputList(BufferedReader br) throws IOException
+    {
+        int n = Integer.parseInt(br.readLine().trim()); // Length of Linked List
+
+        String[] s = br.readLine().trim().split(" ");
+        Node head = new Node(Integer.parseInt(s[0])), tail = head;
+        for(int i = 1; i < s.length; i++)
+            tail = tail.next = new Node(Integer.parseInt(s[i]));
+
+        return head;
+    }
+
+    public static void printList(Node node)
+    {
+        while (node != null)
+        {
+    		System.out.print(node.data + " ");
+    		node = node.next;
+    	}
+    	System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            Node head = Node.inputList(br);
+            
+            
+            int k;
+            k = Integer.parseInt(br.readLine());
+            
+            Solution obj = new Solution();
+            Node res = obj.reverse(head, k);
+            
+            Node.printList(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+/*
+
+Definition for singly Link List Node
+class Node
+{
+    int data;
+    Node next;
+
+    Node(int x){
+        data = x;
+        next = null;
+    }
+}
+
+You can also use the following for printing the link list.
+Node.printList(Node node);
+*/
+
+class Solution {
+    public static Node reverse(Node head, int k) {
+        // code here
+        Node temp=head;
+        Node temp1=head;
+        while(k>0 && temp!=null){
+            temp1=temp;
+            temp=temp.next;
+            k--;
+        }
+        Node ans=temp1;
+        temp1.next=null;
+        temp1=helper(head);
+        temp=helper(temp);
+        head.next=temp;
+        return ans;
+    }
+    public static Node helper(Node head){
+        Node temp=head;
+        Node temp1=null;
+        while(temp!=null){
+            Node temp2=temp.next;
+            temp.next=temp1;
+            temp1=temp;
+            temp=temp2;
+        }
+        return temp1;
+    }
+}
+        
