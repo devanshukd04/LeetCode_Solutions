b2c3fb6f64d8b407a6621ca6445dd7f2687f6bcf
Devanshu Kishor Dalal <devanshukd27@gmail.com>
2023-03-03 22:47:35 +0530

    Added solution - LeetHub

---
+++ Cutting Rectangles - GFG/cutting-rectangles.java
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0){
+            String input_line[] = read.readLine().trim().split("\\s+");
+            long L = Long.parseLong(input_line[0]);
+            long B = Long.parseLong(input_line[1]);
+
+            Solution ob = new Solution();
+            List<Long> ans = new ArrayList<Long>();
+            ans = ob.minimumSquares(L, B);
+            System.out.print(ans.get(0)+" ");
+            System.out.println(ans.get(1));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static List<Long> minimumSquares(long L, long B)
+    {
+        // code here
+        List<Long> list=new ArrayList<Long>();
+        long ans=gcd(L,B);
+        list.add((long)(L*B)/(ans*ans));
+        list.add((long)ans);
+        return list;
+    }
+    public static long gcd(long a,long b){
+        if(b==0){
+            return a;
+        }
+        return gcd(b,a%b);
+    }
+}
