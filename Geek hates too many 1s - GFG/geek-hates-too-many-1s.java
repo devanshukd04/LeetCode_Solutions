084486510efba2d69a28a2288650e3688537880e
Devanshu Kishor Dalal <devanshukd27@gmail.com>
2023-03-06 18:00:02 +0530

    Added solution - LeetHub

---
+++ Geek hates too many 1s - GFG/geek-hates-too-many-1s.java
@@ -0,0 +1,66 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int n;
+            n = Integer.parseInt(br.readLine());
+            
+            Solution obj = new Solution();
+            int res = obj.noConseBits(n);
+            
+            System.out.println(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public static int noConseBits(int n) {
+        // code here
+        List<Integer> list=new ArrayList<Integer>();
+        while(n>0){
+            list.add(n&1);
+            n>>=1;
+        }
+        for(int i=0;i<list.size()/2;i++){
+            int temp=list.get(i);
+            list.set(i,list.get(list.size()-1-i));
+            list.set(list.size()-1-i,temp);
+        }
+        boolean flag=false;
+        boolean flag1=false;
+        int ans=0;
+        for(int i=0;i<list.size();i++){
+            int bit=list.get(i);
+            if(!flag1 && !flag && bit==1){
+                flag=true;
+            }
+            else if(!flag1 && flag && bit==1){
+                flag1=true;
+            }
+            else if(flag1 && flag && bit==1){
+                flag=false;
+                flag1=false;
+                bit=0;
+            }
+            else{
+                flag=false;
+                flag1=false;
+            }
+            ans<<=1;
+            ans|=bit;
+        }
+        return ans;
+    }
+}
+        
