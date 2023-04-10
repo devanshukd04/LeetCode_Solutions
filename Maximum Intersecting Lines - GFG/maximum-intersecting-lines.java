fb7ab5e700bf7b5b314c2cac87431f6b9ca4aef5
Devanshu Kishor Dalal <devanshukd27@gmail.com>
2023-04-10 19:30:20 +0530

    Added solution - LeetHub

---
+++ Maximum Intersecting Lines - GFG/maximum-intersecting-lines.java
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+// Initial Template for Java
+// Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while (t-- > 0) {
+
+            int i = 0;
+            int N = Integer.parseInt(br.readLine().trim());
+
+            int lines[][] = new int[(int)(N)][2];
+
+            for (int j = 0; j < 2; j++) {
+                String inputLine2[] = br.readLine().trim().split(" ");
+                for (i = 0; i < N; i++) {
+                    lines[i][j] = Integer.parseInt(inputLine2[i]);
+                }
+            }
+            Solution ob = new Solution();
+            System.out.println(ob.maxIntersections(lines, N));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int maxIntersections(int[][] lines, int N) {
+        // Code here
+        List<Integer> list=new ArrayList<Integer>();
+        HashMap<Integer,Integer> map=new HashMap<Integer,Integer>();
+        for(int i=0;i<lines.length;i++){
+            map.put(lines[i][0],map.getOrDefault(lines[i][0],0)+1);
+            map.put(lines[i][1]+1,map.getOrDefault(lines[i][1]+1,0)-1);
+        }
+        Set<Integer> set=map.keySet();
+        for(int num:set){
+            list.add(num);
+        }
+        Collections.sort(list);
+        int ans=0;
+        int temp=0;
+        for(int num:list){
+            temp+=map.get(num);
+            ans=ans<temp?temp:ans;
+        }
+        return ans;
+    }
+}
