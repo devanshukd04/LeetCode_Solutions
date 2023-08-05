fbe0511e1a8f2bbda6b48cc6f5e4a57a7a3c10c0
Devanshu Kishor Dalal <devanshukd27@gmail.com>
2023-08-05 18:01:54 +0530

    Time: 1 ms (99.88%), Space: 43.7 MB (53.68%) - LeetHub

---
+++ 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<TreeNode> allPossibleBST(int start, int end,
+            Map<Pair<Integer, Integer>, List<TreeNode>> memo) {
+        List<TreeNode> res = new ArrayList<>();
+        if (start > end) {
+            res.add(null);
+            return res;
+        }
+        if (memo.containsKey(new Pair<>(start, end))) {
+            return memo.get(new Pair<>(start, end));
+        }
+        // Iterate through all values from start to end to construct left and right subtree recursively.
+        for (int i = start; i <= end; ++i) {
+            List<TreeNode> leftSubTrees = allPossibleBST(start, i - 1, memo);
+            List<TreeNode> rightSubTrees = allPossibleBST(i + 1, end, memo);
+
+            // Loop through all left and right subtrees and connect them to ith root.
+            for (TreeNode left: leftSubTrees) {
+                for (TreeNode right: rightSubTrees) {
+                    TreeNode root = new TreeNode(i, left, right);
+                    res.add(root);
+                }
+            }
+        }
+        memo.put(new Pair<>(start, end), res);
+        return res;
+    }
+
+    public List<TreeNode> generateTrees(int n) {
+        Map<Pair<Integer, Integer>, List<TreeNode>> memo = new HashMap<>();
+        return allPossibleBST(1, n, memo);
+    }
+}
