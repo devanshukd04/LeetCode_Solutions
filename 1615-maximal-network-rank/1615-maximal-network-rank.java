34b0903df922facbd54be87ae5c3c2042a8f2d46
Devanshu Kishor Dalal <devanshukd27@gmail.com>
2023-08-18 10:06:33 +0530

    Time: 24 ms (45.73%), Space: 43.7 MB (93.63%) - LeetHub

---
+++ 1615-maximal-network-rank/1615-maximal-network-rank.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int maximalNetworkRank(int n, int[][] roads) {
+        Map<Integer,HashSet<Integer>> map=new HashMap<Integer,HashSet<Integer>>();
+        int ans=0;
+        int[] count=new int[n];
+        for(int i=0;i<roads.length;i++){
+            int pointA=roads[i][0];
+            int pointB=roads[i][1];
+            if(!map.containsKey(pointA)){
+                map.put(pointA,new HashSet<Integer>());
+            }
+            if(!map.containsKey(pointB)){
+                map.put(pointB,new HashSet<Integer>());
+            }
+            map.get(pointA).add(pointB);
+            map.get(pointB).add(pointA);
+            count[pointA]++;
+            count[pointB]++;
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i!=j){
+                    if(map.containsKey(i) && map.get(i).contains(j)){
+                        ans=Math.max(ans,count[i]+count[j]-1);
+                    }
+                    else{
+                        ans=Math.max(ans,count[i]+count[j]);
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+}
