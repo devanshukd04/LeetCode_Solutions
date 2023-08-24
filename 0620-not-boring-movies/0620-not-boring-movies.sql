79148b65dd9a4c46a321215a92465619491c4f8e
Devanshu Kishor Dalal <devanshukd27@gmail.com>
2023-08-24 10:38:16 +0530

    Time: 776 ms (5.01%), Space: 0B (100.00%) - LeetHub

--- 0620-not-boring-movies/0620-not-boring-movies.sql
+++ 0620-not-boring-movies/0620-not-boring-movies.sql
@@ -1,2 +1,2 @@
 # Write your MySQL query statement below
-select * from Cinema where id%2=1 && description!='boring' order by rating desc;
+select * from Cinema where mod(id,2)=1 && description!='boring' order by rating desc;
