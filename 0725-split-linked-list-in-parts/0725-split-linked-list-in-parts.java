7593d5a595cc5485ea61e6a6bc19d58344d732b5
Devanshu Kishor Dalal <devanshukd27@gmail.com>
2023-09-06 11:53:54 +0530

    Time: 0 ms (100.00%), Space: 43.8 MB (27.02%) - LeetHub

---
+++ 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode[] splitListToParts(ListNode head, int k) {
+        ListNode[] ans=new ListNode[k];
+        ListNode temp=head;
+        int length=0;
+        while(temp!=null){
+            temp=temp.next;
+            length++;
+        }
+        int partSize=length/k;
+        int extra=length%k;
+        temp=head;
+        ListNode prev=temp;
+        for(int i=0;i<k;i++){
+            if(temp==null){
+                ans[i]=null;
+            }
+            else{
+                int prevSize=partSize;
+                ans[i]=temp;
+                while(temp!=null && prevSize>0){
+                    prev=temp;
+                    temp=temp.next;
+                    prevSize--;
+                }
+                if(extra>0){
+                    extra--;
+                    prev=temp;
+                    temp=temp.next;
+                }
+                prev.next=null;
+            }
+        }
+        return ans;
+    }
+}
