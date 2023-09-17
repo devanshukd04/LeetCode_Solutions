3a7dee9676439954c32e6f15f8202be69607ee83
Devanshu Kishor Dalal <devanshukd27@gmail.com>
2023-09-17 20:12:36 +0530

    Time: 79 ms (15.19%), Space: 49.4 MB (21.48%) - LeetHub

---
+++ 0847-shortest-path-visiting-all-nodes/0847-shortest-path-visiting-all-nodes.java
@@ -0,0 +1,46 @@
+class Solution {
+    class Node{
+        int vertex;
+        int mask;
+        int cost;
+        Node(int vertex, int mask, int cost){
+            this.vertex=vertex;
+            this.mask=mask;
+            this.cost=cost;
+        }
+    }
+    public int shortestPathLength(int[][] graph) {
+        int nodes=graph.length;
+        int finMask=(1<<nodes)-1;
+        
+        Set<Pair<Integer,Integer>> visited=new HashSet<>();
+        Queue<Node> queue=new LinkedList<>();
+        
+        for(int i=0;i<nodes;i++){
+            int mask=(1<<i);
+            Node node=new Node(i,mask,0);
+            queue.add(node);
+            visited.add(new Pair<>(i,mask));
+        }
+        
+        while(!queue.isEmpty()){
+            Node node=queue.poll();
+            if(node.mask==finMask){
+                return node.cost;
+            }
+            for(int i=0;i<graph[node.vertex].length;i++){
+                int targetVertex=graph[node.vertex][i];
+                int targetMask=(1<<targetVertex);
+                int destMask=node.mask | targetMask;
+                
+                if(visited.contains(new Pair<>(targetVertex,destMask))){
+                    continue;
+                }
+                visited.add(new Pair<>(targetVertex,destMask));
+                queue.add(new Node(targetVertex,destMask,node.cost+1));
+            }
+        }
+        
+        return -1;
+    }
+}
