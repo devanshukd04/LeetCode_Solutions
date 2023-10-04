f885c4986f3c6db391569fb32f74e3733b2c5eea
Devanshu Kishor Dalal <devanshukd27@gmail.com>
2023-10-04 21:17:01 +0530

    Time: 291 ms (7.69%), Space: 46.2 MB (88.70%) - LeetHub

---
+++ 0706-design-hashmap/0706-design-hashmap.java
@@ -0,0 +1,38 @@
+class MyHashMap {
+    List<Integer> keys;
+    List<Integer> values;
+    public MyHashMap() {
+        this.keys=new ArrayList<Integer>();
+        this.values=new ArrayList<Integer>();
+    }
+    
+    public void put(int key, int value) {
+        int a;
+        if(keys.contains(key)){
+            a=keys.indexOf(key);
+            values.set(a,value);
+        }
+        else{
+            keys.add(key);
+            values.add(value);
+        }
+    }
+    
+    public int get(int key) {
+        int a=-1;
+        if(keys.contains(key)){
+            a=keys.indexOf(key);
+            a=values.get(a);
+        }
+        return a;
+    }
+    
+    public void remove(int key) {
+        int a;
+        if(keys.contains(key)){
+            a=keys.indexOf(key);
+            keys.remove(a);
+            values.remove(a);
+        }
+    }
+}
