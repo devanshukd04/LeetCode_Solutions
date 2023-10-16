ed92c144e375e15b0657d13772b60cf2156eec1d
Devanshu Kishor Dalal <devanshukd27@gmail.com>
2023-10-16 13:19:37 +0530

    Time: 607 ms (80.78%), Space: 0B (100.00%) - LeetHub

--- 1978-employees-whose-manager-left-the-company/1978-employees-whose-manager-left-the-company.sql
+++ 1978-employees-whose-manager-left-the-company/1978-employees-whose-manager-left-the-company.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
 
-select employee_id from Employees where salary<30000 and manager_id in 
-(select manager_id from Employees where manager_id not in (select employee_id from Employees))
+select employee_id from Employees where salary<30000 and manager_id not in 
+(select employee_id from Employees)
 order by employee_id
