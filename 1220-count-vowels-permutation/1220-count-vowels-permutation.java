02c29cd31caa9421691621ac78396c1af3974971
Devanshu Kishor Dalal <devanshukd27@gmail.com>
2023-10-28 21:08:05 +0530

    Time: 44 ms (16.83%), Space: 49.6 MB (16.50%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.java
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.java
@@ -1,36 +1,106 @@
 class Solution {
+    int mod=(int)1e9+7;
     public int countVowelPermutation(int n) {
-        int mod=(int)1e9+7;
+        Map<Character,List<Character>> map=new HashMap<>();
+        
+        List<Character> list1=new ArrayList<>();
+        list1.add('a');
+        list1.add('e');
+        list1.add('i');
+        list1.add('o');
+        list1.add('u');
+        map.put('s',list1);
+        
+        List<Character> list2=new ArrayList<>();
+        list2.add('e');
+        map.put('a',list2);
+        
+        List<Character> list3=new ArrayList<>();
+        list3.add('a');
+        list3.add('i');
+        map.put('e',list3);
+        
+        List<Character> list4=new ArrayList<>();
+        list4.add('a');
+        list4.add('e');
+        list4.add('o');
+        list4.add('u');
+        map.put('i',list4);
+        
+        List<Character> list5=new ArrayList<>();
+        list5.add('i');
+        list5.add('u');
+        map.put('o',list5);
+        
+        List<Character> list6=new ArrayList<>();
+        list6.add('a');
+        map.put('u',list6);
+        
+        int[][] dp=new int[n+1][6];
+        for(int[] arr:dp){
+            Arrays.fill(arr,-1);
+        }
+        
+        return helper(map,dp,n,0);
+    }
+    
+    public int helper(Map<Character,List<Character>> map, int[][] dp, int n, int prev){
+        if(n==0){
+            return 1;
+        }
+        
+        if(dp[n][prev]!=-1){
+            return dp[n][prev];
+        }
+        
+        char key=getChar(prev);
         int ans=0;
-        int[][] dp=new int[n][5];
-        for(int i=0;i<5;i++){
-            dp[n-1][i]=1;
+        
+        for(Character ch:map.get(key)){
+            int newPrev=getCharNum(ch);
+            ans=(ans+helper(map,dp,n-1,newPrev))%mod;
         }
-        for(int i=n-2;i>=0;i--){
-            for(int j=0;j<5;j++){
-                if(j==0){
-                    dp[i][j]=dp[i+1][1];
-                }
-                else if(j==1){
-                    dp[i][j]=(dp[i+1][0]+dp[i+1][2])%mod;
-                }
-                else if(j==2){
-                    dp[i][j]=(dp[i][j]+dp[i+1][0])%mod;
-                    dp[i][j]=(dp[i][j]+dp[i+1][1])%mod;
-                    dp[i][j]=(dp[i][j]+dp[i+1][3])%mod;
-                    dp[i][j]=(dp[i][j]+dp[i+1][4])%mod;
-                }
-                else if(j==3){
-                    dp[i][j]=(dp[i+1][2]+dp[i+1][4])%mod;
-                }
-                else{
-                    dp[i][j]=dp[i+1][0];
-                }
-            }
+        
+        return dp[n][prev]=ans;
+    }
+    public char getChar(int prev){
+        if(prev==0){
+            return 's';
         }
-        for(int j=0;j<5;j++){
-            ans=(ans+dp[0][j])%mod;
+        else if(prev==1){
+            return 'a';
         }
-        return ans;
+        else if(prev==2){
+            return 'e';
+        }
+        else if(prev==3){
+            return 'i';
+        }
+        else if(prev==4){
+            return 'o';
+        }
+        else{
+            return 'u';
+        }
+    }
+    public int getCharNum(char ch){
+        if(ch=='s'){
+            return 0;
+        }
+        else if(ch=='a'){
+            return 1;
+        }
+        else if(ch=='e'){
+            return 2;
+        }
+        else if(ch=='i'){
+            return 3;
+        }
+        else if(ch=='o'){
+            return 4;
+        }
+        else{
+            return 5;
+        }
     }
 }
